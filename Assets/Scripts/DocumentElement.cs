38d3e94942d863b4e4739376635ea2938a7f39f1
Luc Arnould <luc.arnould08@gmail.com>
2023-04-21 12:18:48 +0200

    Ajout du support multi panels
    Ajout d'un script de transition des panels
    Ajout d'un début de panel pour les descriptions
    Boutons fonctionnels mais pas encore liés

--- Assets/Scripts/DocumentElement.cs
+++ Assets/Scripts/DocumentElement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static PanelTransiter;
 
 public class DocumentElement : MonoBehaviour
 {
@@ -10,16 +11,21 @@ public class DocumentElement : MonoBehaviour
 
     public Document document { get; private set; }
 
+    private DocumentPanelController documentPanelController;
+
     public void SetDocument(Document document)
     {
         this.document = document;
         documentIcon.sprite = document.documentIcon;
     }
 
+    public void SetDocumentPanelController(DocumentPanelController documentPanelController)
+    {
+        this.documentPanelController = documentPanelController;
+    }
+
     public void OnClick()
     {
-        //TODO: Open in vizualizer
-
-        Debug.Log("Document " + document.documentName + " clicked!");
+        documentPanelController.ShowDocument(document);
     }
 }
---
+++ Assets/Scripts/PanelTransiter.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.TimeZoneInfo;
+
+public class PanelTransiter : MonoBehaviour
+{
+    public enum TransitionType
+    {
+        SlideIn,
+        SlideOut
+    }
+
+    [Header("References")]
+    [SerializeField] private RectTransform panelRectTransform;
+
+    [Header("Settings")]
+    [SerializeField] private float transitionTime = 0.5f;
+
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+
+    private bool isTransitioning = false;
+
+    public bool IsReady => !isTransitioning;
+
+    public Action OnTransitionStart;
+    public Action OnTransitionComplete;
+
+    private void Start()
+    {
+        startPosition = new Vector2(0f, -Screen.height);
+        endPosition = new Vector2(0f, 0f);
+
+        panelRectTransform.anchoredPosition = startPosition;
+    }
+
+    public void StartTransition(TransitionType type)
+    {
+        if (isTransitioning)
+            return;
+
+        StartCoroutine(MakeTransition(type));
+    }
+
+    private IEnumerator MakeTransition(TransitionType type)
+    {
+        isTransitioning = true;
+
+        OnTransitionStart?.Invoke();
+
+        float transitionFactor = type == TransitionType.SlideIn ? 1f : -1f;
+
+        Vector2 from = type == TransitionType.SlideIn ? startPosition : endPosition;
+        Vector2 to = type == TransitionType.SlideIn ? endPosition : startPosition;
+
+        float transitionTimer = 0f;
+        float transitionProgress = 0f;
+        while (transitionTimer < transitionTime)
+        {
+            transitionTimer += Time.deltaTime;
+            transitionProgress = (1 - Mathf.Cos(transitionTimer / transitionTime * Mathf.PI)) * .5f;
+            
+            panelRectTransform.anchoredPosition = Vector3.Slerp(startPosition, endPosition, transitionProgress);
+
+            yield return null;
+        }
+
+        panelRectTransform.anchoredPosition = endPosition;
+
+        isTransitioning = false;
+
+        OnTransitionComplete?.Invoke();
+    }
+}
