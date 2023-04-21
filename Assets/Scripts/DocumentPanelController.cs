da433bd0cca6197ed05662c3f87efcf5c211eea6
Luc Arnould <luc.arnould08@gmail.com>
2023-04-21 16:30:26 +0200

    Support de date
    Ajout du support de date

--- Assets/Scripts/DocumentPanelController.cs
+++ Assets/Scripts/DocumentPanelController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class DocumentPanelController : PanelBase
 {
     [Header("References")]
     [SerializeField] private TMP_Text nameTxt;
     [SerializeField] private TMP_Text descriptionTxt;
+    [SerializeField] private TMP_Text dateTxt;
     [SerializeField] private Image iconImg;
     [SerializeField] InventoryPanelController inventoryPanelController;
 
@@ -16,8 +18,11 @@ public class DocumentPanelController : PanelBase
 
     private void RefreshGraphics()
     {
+        DateTime date = new DateTime(InventorySaver.Instance.GetDocument(document).obtainedOn);
+
         nameTxt.text = document ? document.documentName : string.Empty;
         descriptionTxt.text = document ? document.documentDescription : string.Empty;
+        dateTxt.text = document ? $"Le {date.ToString("dd/MM/yy")}" : string.Empty;
         iconImg.sprite = document ? document.documentIcon : null;
         iconImg.color = document ? Color.white : Color.clear;
     }
